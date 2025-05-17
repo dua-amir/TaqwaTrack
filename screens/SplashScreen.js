f7d6dc7706992e01ec66b84a417a1eb8b24057f2
dua-amir <duaamir4211@gmail.com>
2025-05-17 22:14:18 +0500

    updated screens folder

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,52 +1,10 @@
-/*
-import React, { useEffect } from 'react';
-import { View, Text, ActivityIndicator, StyleSheet, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-
-const SplashScreen = () => {
-  const navigation = useNavigation();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      navigation.replace('Login');
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Image source={require('../assets/images/logo.png')} style={styles.logo} />
-      <ActivityIndicator size="large" color="#6A0DAD" />
-      <Text>Loading...</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  logo: {
-    width: 200,
-    height: 200,
-    marginBottom: 20,
-  },
-});
-
-export default SplashScreen;
-*/
-
 
 // SplashScreen.js
 import React, { useEffect } from 'react';
 import { View, Text, ActivityIndicator, StyleSheet, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { colors } from '../styles/colors'; // optional
+import { colors } from '../styles/colors'; 
 
 const SplashScreen = () => {
   const navigation = useNavigation();
